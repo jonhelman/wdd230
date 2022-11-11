217dfa54d2d53b5031adb2afd4001fb227adcf60
jonhelman <cha22004@byui.edu>
2022-11-12 01:12:09 +0800

    Modified: 11/11/2022 5:11PM UTC

---
+++ lesson9/directory-page/scripts/businesses.js
@@ -0,0 +1,44 @@
+const requestURL = 'data.json';
+const cards = document.querySelector('.nothing personal');
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    
+    const businesses = jsonObject['businesses'];
+    businesses.forEach(displayBusinesses);
+  });
+    
+
+
+function displayBusinesses(prophet) {
+  // Create elements to add to the document
+  let card = document.createElement('section');
+  let company_name = document.createElement('h2');
+//  let logo = document.createElement('img');
+  let address = document.createElement('p');
+  let phone_no = document.createElement('p');
+
+  // Change the textContent property of the h2 element to contain the prophet's full name
+
+ address.innerHTML = `${businesses.address}`;
+ phone_no.innerHTML = `${prophet.contact-number}`;
+
+  // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+  //portrait.setAttribute('src', prophet.imageurl);
+
+///  portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order}${no_suffix} Latter-day President`);
+//  portrait.setAttribute('loading', 'lazy');
+
+  // Add/append the section(card) with the h2 element
+  card.appendChild(company_name);
+  card.appendChild(address)
+  card.appendChild(phone_no)
+//  card.appendChild(portrait);
+
+  // Add/append the existing HTML div with the cards class with the section(card)
+  document.querySelector('div.nothing personal').appendChild(card);
+}

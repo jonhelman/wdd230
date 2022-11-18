a5e5dc2d4731a62815ad64985c866a7c00d86c7d
jonhelman <cha22004@byui.edu>
2022-11-18 09:57:55 +0800

    ...again

--- lesson9/directory-page/scripts/businesses.js
+++ lesson9/directory-page/scripts/businesses.js
@@ -24,7 +24,7 @@ function displayBusinesses(businesses) {
   let site = document.createElement('a');
 
   let membership = document.createElement("p")
-z
+
   // Change the textContent property of the h2 element to contain the prophet's full name
 
  address.innerHTML = `${businesses.address}`;

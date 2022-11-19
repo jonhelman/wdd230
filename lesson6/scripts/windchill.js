aab75379e4041074b69ca202f2823ad72ec12709
jonhelman <cha22004@byui.edu>
2022-11-19 20:22:05 +0800

    V1 of Join Page Completed

---
+++ lesson6/scripts/windchill.js
@@ -0,0 +1,25 @@
+function set_weather_data(){
+    const T = parseFloat(document.getElementById('temperature').textContent);
+    const V = parseFloat(document.getElementById('wspeed').textContent);
+    
+    const F = document.getElementById('wchill');
+
+    let windchill = 0; 
+
+    if (T <= 10 && V > 4.8){
+
+        windchill = 13.12 + 0.6215 * T - 11.37 * V ** 0.16 + 0.3965 * T * V ** 0.16;
+        windchill = windchill.toFixed(1);
+
+    }else{
+
+        windchill = "N/A";
+    
+    }
+    
+    F.textContent = windchill;
+
+}
+
+
+set_weather_data();

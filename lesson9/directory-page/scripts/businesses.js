25de0f35fab4a633c47bbbb4f240c5fe6e321770
jonhelman <cha22004@byui.edu>
2022-11-19 18:52:39 +0800

    Join Page Overhauled

--- lesson9/directory-page/scripts/businesses.js
+++ lesson9/directory-page/scripts/businesses.js
@@ -1,5 +1,5 @@
 const requestURL = 'https://jonhelman.github.io/wdd230/lesson9/directory-page/scripts/data.json';
-const cards = document.querySelector('.nothing personal');
+const cards = document.querySelector('.grid');
 
 fetch(requestURL)
   .then(function (response) {
@@ -19,6 +19,7 @@ function displayBusinesses(businesses) {
   let card = document.createElement('section');
  // let company = document.createElement('h2');
   let logo = document.createElement('img');
+  let title = document.createElement('p')
   let address = document.createElement('p');
   let phone_no = document.createElement('p');
   let site = document.createElement('a');
@@ -27,10 +28,11 @@ function displayBusinesses(businesses) {
 
   // Change the textContent property of the h2 element to contain the prophet's full name
 
+ title.innerHTML = `<strong class = "title">${businesses.company}</strong>`;
  address.innerHTML = `${businesses.address}`;
  phone_no.innerHTML = `${businesses.phone}`;
  site.innerHTML = `${businesses.website}`;
- membership.innerHTML = `<strong class="${businesses.level}">${businesses.level} Membership</strong>`;
+ membership.innerHTML = `<strong class="${businesses.level}">${businesses.level}</strong>`;
 
 
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
@@ -43,11 +45,32 @@ function displayBusinesses(businesses) {
 
   // Add/append the section(card) with the h2 element
   card.appendChild(logo)
+  card.appendChild(title)
   card.appendChild(address)
   card.appendChild(phone_no)
   card.appendChild(site)
   card.appendChild(membership)
 
   // Add/append the existing HTML div with the cards class with the section(card)
-  document.querySelector('div.nothing-personal').appendChild(card);
+  document.querySelector('div.grid').appendChild(card);
+
+//Grid-list
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".grid");
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
+
 }
